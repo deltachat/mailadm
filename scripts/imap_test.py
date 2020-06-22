e83ad5c6762b33f845487704f28b1dd097caddb0
holger krekel <holger@merlinux.eu>
2020-06-22 21:25:44 +0200

    add a small test script

---
+++ scripts/imap_test.py
@@ -0,0 +1,44 @@
+
+import email
+import smtplib
+from textwrap import dedent
+from imapclient import IMAPClient
+
+
+def receive_imap(host, user, password, num):
+    conn = IMAPClient(host)
+    conn.login(user, password)
+    info = conn.select_folder("INBOX")
+    print(info)
+    messages = conn.fetch("1:*", [b'FLAGS'])
+    latest_msg = max(messages)
+    requested = "FLAGS BODY.PEEK[]"
+    for uid, data in conn.fetch([latest_msg], [b"FLAGS", b"BODY.PEEK[]"]).items():
+        body_bytes = data[b'BODY[]']
+        email_message = email.message_from_bytes(body_bytes)
+        assert email_message["subject"] == str(num)
+        print("received message num={}".format(num))
+        print(email_message)
+
+
+def send_self_mail(host, user, password, num):
+    smtp = smtplib.SMTP_SSL(host)
+    smtp.connect(host)
+    smtp.login(user, password)
+    msg = """\
+From: {user}
+To: {user}
+Subject: {num}
+
+hi there
+""".format(user=user, num=num)
+    smtp.sendmail(user, [user], msg)
+    print("send message num={}".format(num))
+
+if __name__ == "__main__":
+    import sys
+    user, password = sys.argv[1:]
+    host = user.split("@", 1)[-1]
+    num = 42
+    send_self_mail(host, user, password, num)
+    receive_imap(host, user, password, num)
