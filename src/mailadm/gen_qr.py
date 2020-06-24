6ad640c71e24041a8b243f83771109f552d38b2d
holger krekel <holger@merlinux.eu>
2020-06-24 14:26:26 +0200

    with both logos

--- src/mailadm/gen_qr.py
+++ src/mailadm/gen_qr.py
@@ -21,7 +21,7 @@ def gen_qr(config, token_info):
 
     qr = qrcode.QRCode(
         version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_Q,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
         box_size=1,
         border=1,
     )
@@ -39,12 +39,13 @@ def gen_qr(config, token_info):
     text_margin_right = 6
 
     ttf_path = pkg_resources.resource_filename('mailadm', 'data/opensans-regular.ttf')
-    logo_path = pkg_resources.resource_filename('mailadm', 'data/delta-chat-bw.png')
+    logo_bw_path = pkg_resources.resource_filename('mailadm', 'data/delta-chat-bw.png')
+    logo_red_path = pkg_resources.resource_filename('mailadm', 'data/delta-chat-red.png')
     assert os.path.exists(ttf_path), ttf_path
 
     font = ImageFont.truetype(font=ttf_path, size=font_size)
 
-    logo_img = Image.open(logo_path)
+    logo_img = Image.open(logo_bw_path)
 
     image = Image.new("RGBA", (width, height), "white")
 
@@ -53,13 +54,22 @@ def gen_qr(config, token_info):
     qr_final_size = width - (qr_padding * 2)
     logo_width = int(qr_final_size / 4)
 
+    # draw text
     draw.multiline_text((text_margin_right + logo_width, height - font_height), steps + info,
                         font=font, fill="black", align="left")
 
+    # paste QR code
     image.paste(qr_img.resize((qr_final_size, qr_final_size), resample=Image.NEAREST),
                 (qr_padding, qr_padding))
 
     logo = logo_img.resize((logo_width, logo_width), resample=Image.NEAREST)
     image.paste(logo, (0, qr_final_size + qr_padding), mask=logo)
+
+    # red background delta logo
+    logo2_img = Image.open(logo_red_path)
+    logo2_width = int(size / 6)
+    logo2 = logo2_img.resize((logo2_width, logo2_width), resample=Image.NEAREST)
+    pos = int((size / 2) - (logo2_width / 2))
+    image.paste(logo2, (pos, pos), mask=logo2)
 
     return image
