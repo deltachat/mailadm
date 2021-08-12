725d98dd8a02d9f125c94085162797ef6fcd67f8
missytake <missytake@systemli.org>
2021-08-12 19:52:05 +0200

    refactoring bot.py

--- src/mailadm/bot.py
+++ src/mailadm/bot.py
@@ -16,29 +16,26 @@ class AdmBot:
 
     @account_hookimpl
     def ac_incoming_message(self, command):
-        print("process_incoming message", command)
+        print("process_incoming message:", command)
+        chat = command.message.chat
         if command.text.strip() == "/help":
             command.create_chat()
             text = "/add_token name expiry prefix token maxuse\n/add_user addr password token"
             command.chat.send_text(text)
 
         elif command.text.strip() == "/add-token":
-            chat = command.message.chat
-            if chat.is_group() and int(self.admingrpid) == chat.id:
-                if chat.is_protected() and int(chat.num_contacts) >= 2:
-                    command.create_chat()
-                    arguments = command.text.split(" ")
-                    text = cmdline.add_token(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4])
-                    command.chat.send_text(text)
+            if self.check_privileges(chat):
+                command.create_chat()
+                arguments = command.text.split(" ")
+                text = cmdline.add_token(arguments[0], arguments[1], arguments[2], arguments[3], arguments[4])
+                command.chat.send_text(text)
 
         elif command.text.strip() == "/add-user":
-            chat = command.message.chat
-            if chat.is_group() and int(self.admingrpid) == chat.id:
-                if chat.is_protected() and int(chat.num_contacts) >= 2:
-                    command.create_chat()
-                    arguments = command.text.split(" ")
-                    text = cmdline.add_token(arguments[0], arguments[1], arguments[2])
-                    command.chat.send_text(text)
+            if self.check_privileges(chat):
+                command.create_chat()
+                arguments = command.text.split(" ")
+                text = cmdline.add_token(arguments[0], arguments[1], arguments[2])
+                command.chat.send_text(text)
         else:
             # unconditionally accept the chat
             command.create_chat()
@@ -51,7 +48,21 @@ class AdmBot:
 
     @account_hookimpl
     def ac_message_delivered(self, message):
-        print("ac_message_delivered", message)
+        print("ac_message_delivered:", message)
+
+    def check_privileges(self, chat):
+        """
+        Checks whether the incoming message was in the admin group.
+        """
+        if chat.is_group() and self.admingrpid == chat.id:
+            if chat.is_protected() and int(chat.num_contacts) >= 2:
+                return True
+            else:
+                print("admin chat is broken. Group ID:" + self.admingrpid)
+                raise Exception
+        else:
+            # reply "This command needs to be sent to the admin group"
+            return False
 
 
 def main(argv=None):
