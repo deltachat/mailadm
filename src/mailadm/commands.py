cd8a72142da9b3e74b8300e307d63c6c6fcb42c2
missytake <missytake@systemli.org>
2021-08-19 16:43:13 +0200

    finishing add_token command

--- src/mailadm/commands.py
+++ src/mailadm/commands.py
@@ -1,12 +1,26 @@
 from mailadm.util import get_human_readable_id
-from mailadm.conn import DBError
+from mailadm.db import write_connection
 
 
-def add_token(name, expiry, maxuse, prefix, token, db):
+def add_token(name, expiry, maxuse, prefix, token):
     if token is None:
         token = expiry + "_" + get_human_readable_id(len=15)
-    with db.write_transaction() as conn:
+    with write_connection() as conn:
         info = conn.add_token(name=name, token=token, expiry=expiry,
                               maxuse=maxuse, prefix=prefix)
         tc = conn.get_tokeninfo_by_name(info.name)
-        dump_token_info(tc)
+        return dump_token_info(tc)
+
+
+def dump_token_info(token_info):
+    """Format token info into a string
+    """
+    return """token: {}
+  address prefix: {}
+  accounts expire after: {}
+  token was used {} of {} times
+  token: {}
+    - url: 
+    - 
+    """.format(token_info.name, token_info.prefix, token_info.expiry, token_info.usecount, token_info.maxuse,
+               token_info.token, token_info.get_web_url(), token_info.get_qr_url())
