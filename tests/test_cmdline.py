13a2a6c843b00b6c2db34a7c1b2999fb11b9cafd
missytake <missytake@systemli.org>
2021-10-24 18:08:10 +0200

    tests: add_token has different output now

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -5,7 +5,7 @@ import pytest
 
 
 @pytest.fixture
-def mycmd(request, cmd, make_db, tmpdir, monkeypatch):
+def mycmd(cmd, make_db, tmpdir, monkeypatch):
     db = make_db(tmpdir.mkdir("mycmd"), init=False)
     monkeypatch.setenv("MAILADM_DB", str(db.path))
     cmd.db = db
@@ -82,12 +82,11 @@ class TestTokens:
             *DCACCOUNT*&n=test1
         """)
         out = mycmd.run_ok(["list-tokens"], """
-            *maxuse*50*
-            *usecount*
+            *of 50 times*
             *DCACCOUNT*&n=test1
         """)
         for line in out.splitlines():
-            parts = line.split("=")
+            parts = line.split(":")
             if len(parts) >= 2 and parts[0].strip() == "token":
                 token = parts[1].strip().replace("_", "")
                 assert token.isalnum()
@@ -103,16 +102,16 @@ class TestTokens:
 
     def test_tokens_add_maxuse(self, mycmd):
         mycmd.run_ok(["add-token", "test1", "--maxuse=10"], """
-            *maxuse*10
+            *of 10 times*
             *DCACCOUNT*&n=test1
         """)
         mycmd.run_ok(["list-tokens"], """
-            *maxuse*10*
+            *of 10 times*
             *DCACCOUNT*&n=test1
         """)
         mycmd.run_ok(["mod-token", "--maxuse=1000", "test1"])
         mycmd.run_ok(["list-tokens"], """
-            *maxuse*1000*
+            *of 1000 times*
             *DCACCOUNT*&n=test1
         """)
 
