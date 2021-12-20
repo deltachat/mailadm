372cbe69400435b1ab838b8b489bc4fc26239689
missytake <missytake@systemli.org>
2021-12-20 18:08:36 +0100

    more db call refactoring

--- src/mailadm/cmdline.py
+++ src/mailadm/cmdline.py
@@ -128,7 +128,8 @@ def config(ctx):
 @click.pass_context
 def list_tokens(ctx):
     """list available tokens """
-    click.secho(mailadm.commands.list_tokens())
+    db = get_mailadm_db(ctx)
+    click.secho(mailadm.commands.list_tokens(db))
 
 
 @click.command()
@@ -169,7 +170,7 @@ def add_token(ctx, name, expiry, maxuse, prefix, token):
     from mailadm.util import get_human_readable_id
 
     db = get_mailadm_db(ctx)
-    mailadm.commands.add_token(name, expiry, maxuse, prefix, token, db)
+    click.secho(mailadm.commands.add_token(db, name, expiry, maxuse, prefix, token))
 
 
 @click.command()
@@ -263,7 +264,8 @@ def init(ctx, web_endpoint, mail_domain, mailcow_endpoint, mailcow_token):
 def add_user(ctx, addr, password, token, dryrun):
     """add user as a mailadm managed account.
     """
-    result = mailadm.commands.add_user(token, addr, password, dryrun)
+    db = get_mailadm_db(ctx)
+    result = mailadm.commands.add_user(db, token, addr, password, dryrun)
     if result["status"] == "error":
         ctx.fail(result["message"])
     elif result["status"] == "success":
