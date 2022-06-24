34be36e735abe7ec44bd2f1f5632886c32c574a8
missytake <missytake@systemli.org>
2022-06-24 08:38:34 +0200

    changed errors to json format

--- src/mailadm/web.py
+++ src/mailadm/web.py
@@ -20,16 +20,18 @@ def create_app_from_db(db):
     def new_email():
         token = request.args.get("t")
         if token is None:
-            return "?t (token) parameter not specified", 403
+            return jsonify(type="error", status_code=403,
+                           reason="?t (token) parameter not specified")
 
         with db.write_transaction() as conn:
             token_info = conn.get_tokeninfo_by_token(token)
             if token_info is None:
-                return "token {} is invalid".format(token), 403
+                return jsonify(type="error", status_code=403,
+                               reason="token {} is invalid".format(token))
             try:
                 user_info = conn.add_email_account_tries(token_info, tries=10)
                 return jsonify(email=user_info.addr, password=user_info.password,
                                expiry=token_info.expiry, ttl=user_info.ttl)
             except (DBError, MailcowError) as e:
-                return str(e), 409
+                return jsonify(type="error", status_code=500, reason=str(e))
     return app
