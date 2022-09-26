2e101532948ca9daae8f0aad616fbfc98faa15d5
missytake <missytake@systemli.org>
2022-09-26 14:05:39 +0200

    increased timeout for test_check_privileges, fix lint

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -28,14 +28,14 @@ class TestAdminGroup:
 
     def test_list_tokens(self, admbot, botuser, db):
         admingroup = self.create_admingroup(admbot, botuser, db)
-        command = admingroup.send_text("/list-tokens")
+        admingroup.send_text("/list-tokens")  # command =
         begin = time.time()
         while len(admingroup.get_messages()) < 7:  # this sometimes never completes
             time.sleep(0.1)
             if time.time() > begin + TIMEOUT:
                 pytest.fail("Bot reply took more than %s seconds" % (TIMEOUT,))
         assert admingroup.get_messages()[6].text.startswith("Existing tokens:")
-        #assert admingroup.get_messages()[6].quote == command  # wait for #53
+        # assert admingroup.get_messages()[6].quote == command  # wait for #53
 
     def test_check_privileges(self, admbot, botuser, db):
         self.create_admingroup(admbot, botuser, db)
@@ -44,6 +44,6 @@ class TestAdminGroup:
         begin = time.time()
         while len(direct.get_messages()) < 2:  # this sometimes never completes
             time.sleep(0.1)
-            if time.time() > begin + TIMEOUT:
-                pytest.fail("Bot reply took more than %s seconds" % (TIMEOUT,))
+            if time.time() > begin + TIMEOUT + 10:  # this can take a bit longer, no problem
+                pytest.fail("Bot reply took more than %s seconds" % (TIMEOUT + 10,))
         assert direct.get_messages()[1].text == "Sorry, I only take commands from the admin group."
