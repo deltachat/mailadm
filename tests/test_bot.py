d51b0711c2a9d2bcbd637564cc1bdf883bc01347
missytake <missytake@systemli.org>
2022-09-26 13:43:52 +0200

    made timeout global variable

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -2,6 +2,9 @@ import time
 import pytest
 
 
+TIMEOUT = 30
+
+
 class TestAdminGroup:
     def create_admingroup(self, admbot, botuser, db):
         admchat = admbot.create_group_chat("admins", [], verified=True)
@@ -19,8 +22,8 @@ class TestAdminGroup:
         begin = time.time()
         while len(admingroup.get_messages()) < 7:
             time.sleep(0.1)
-            if time.time() > begin + 20:
-                pytest.fail("Bot reply took more than 20 seconds")
+            if time.time() > begin + TIMEOUT:
+                pytest.fail("Bot reply took more than %s seconds" % (TIMEOUT,))
         assert admingroup.get_messages()[6].text.startswith("/add-user addr password token")
 
     def test_list_tokens(self, admbot, botuser, db):
@@ -29,8 +32,8 @@ class TestAdminGroup:
         begin = time.time()
         while len(admingroup.get_messages()) < 7:  # this sometimes never completes
             time.sleep(0.1)
-            if time.time() > begin + 20:
-                pytest.fail("Bot reply took more than 20 seconds")
+            if time.time() > begin + TIMEOUT:
+                pytest.fail("Bot reply took more than %s seconds" % (TIMEOUT,))
         assert admingroup.get_messages()[6].text.startswith("Existing tokens:")
         #assert admingroup.get_messages()[6].quote == command  # wait for #53
 
@@ -41,6 +44,6 @@ class TestAdminGroup:
         begin = time.time()
         while len(direct.get_messages()) < 2:  # this sometimes never completes
             time.sleep(0.1)
-            if time.time() > begin + 20:
-                pytest.fail("Bot reply took more than 20 seconds")
+            if time.time() > begin + TIMEOUT:
+                pytest.fail("Bot reply took more than %s seconds" % (TIMEOUT,))
         assert direct.get_messages()[1].text == "Sorry, I only take commands from the admin group."
