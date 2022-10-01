70405435f73b34a7602d528d2f4912a577b50fc3
missytake <missytake@systemli.org>
2022-10-01 22:15:12 +0200

    add 1m and 1y as possible shortcodes for expiry dates

--- src/mailadm/util.py
+++ src/mailadm/util.py
@@ -21,7 +21,11 @@ def parse_expiry_code(code):
         raise ValueError("expiry codes are at least 2 characters")
     val = int(code[:-1])
     c = code[-1]
-    if c == "w":
+    if c == "y":
+        return val * 365 * 24 * 60 * 60
+    elif c == "m":
+        return val * 30 * 24 * 60 * 60
+    elif c == "w":
         return val * 7 * 24 * 60 * 60
     elif c == "d":
         return val * 24 * 60 * 60
