93b187b2675a4fe6773842a62cb9eafbd98f768a
missytake <missytake@systemli.org>
2022-10-04 22:14:16 +0200

    use pytest_timeout

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -2,40 +2,34 @@ import time
 import pytest
 
 
-TIMEOUT = 60
+TIMEOUT = 300
 
 
 class TestAdminGroup:
+    @pytest.mark.timeout(TIMEOUT)
     def test_help(self, admingroup):
         num_msgs = len(admingroup.get_messages())
         admingroup.send_text("/help")
-        begin = time.time()
         while len(admingroup.get_messages()) < num_msgs + 2:  # this sometimes never completes
             time.sleep(0.1)
-            if time.time() > begin + TIMEOUT:
-                pytest.fail("Bot reply took more than %s seconds" % (TIMEOUT,))
         reply = admingroup.get_messages()[num_msgs + 1]
         assert reply.text.startswith("/add-user addr password token")
 
+    @pytest.mark.timeout(TIMEOUT)
     def test_list_tokens(self, admingroup):
         num_msgs = len(admingroup.get_messages())
         admingroup.send_text("/list-tokens")  # command =
-        begin = time.time()
         while len(admingroup.get_messages()) < num_msgs + 2:  # this sometimes never completes
             time.sleep(0.1)
-            if time.time() > begin + TIMEOUT:
-                pytest.fail("Bot reply took more than %s seconds" % (TIMEOUT,))
-        reply = admingroup.get_messages()[num_msgs + 2]
+        reply = admingroup.get_messages()[num_msgs + 1]
         assert reply.text.startswith("Existing tokens:")
         # assert reply.quote == command  # wait for #53
 
+    @pytest.mark.timeout(TIMEOUT)
     def test_check_privileges(self, admingroup):
         direct = admingroup.botuser.create_chat(admingroup.admbot.get_config("addr"))
         direct.send_text("/list-tokens")
         num_msgs = len(direct.get_messages())
-        begin = time.time()
         while len(direct.get_messages()) == num_msgs:  # this sometimes never completes
             time.sleep(0.1)
-            if time.time() > begin + TIMEOUT + 10:  # this can take a bit longer, no problem
-                pytest.fail("Bot reply took more than %s seconds" % (TIMEOUT + 10,))
         assert direct.get_messages()[1].text == "Sorry, I only take commands from the admin group."
