59455375fa1b45fc29ce7bdccb376c0e48b843e6
missytake <missytake@systemli.org>
2022-10-05 16:53:23 +0200

    test: relaying messages between user and support group

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,8 +1,9 @@
 import time
 import pytest
+import deltachat
 
 
-TIMEOUT = 60
+TIMEOUT = 90
 
 
 @pytest.mark.timeout(TIMEOUT)
@@ -25,7 +26,7 @@ class TestAdminGroup:
         assert reply.quote == command
 
     def test_check_privileges(self, admingroup):
-        direct = admingroup.botuser.create_chat(admingroup.admbot.get_config("addr"))
+        direct = admingroup.botadmin.create_chat(admingroup.admbot.get_config("addr"))
         direct.send_text("/list-tokens")
         num_msgs = len(direct.get_messages())
         while len(direct.get_messages()) == num_msgs:  # this sometimes never completes
@@ -33,7 +34,55 @@ class TestAdminGroup:
         assert direct.get_messages()[1].text == "Sorry, I only take commands from the admin group."
 
 
+class SupportGroupUserPlugin:
+    def __init__(self, account, botaddr):
+        self.account = account
+        self.account.add_account_plugin(deltachat.events.FFIEventLogger(self.account))
+        self.botaddr = botaddr
+
+    @deltachat.account_hookimpl
+    def ac_incoming_message(self, message: deltachat.Message):
+        message.create_chat()
+
+        assert message.chat.is_protected()
+        assert len(message.chat.get_contacts()) == 2
+        assert message.text == "Can I ask you a support question?"
+        assert message.get_sender_contact().addr == self.botaddr
+
+        message.chat.send_text("I hope the user can't read this")
+        print("sent secret message")
+        reply = deltachat.Message.new_empty(self.account, "text")
+        reply.set_text("Yes of course you can ask us :)")
+        reply.quote = message
+        message.chat.send_msg(reply)
+        print("sent public message")
+
+
 class TestSupportGroup:
+    @pytest.mark.timeout(TIMEOUT)
+    def test_support_group_relaying(self, admingroup, supportuser):
+        supportchat = supportuser.create_chat(admingroup.admbot.get_config("addr"))
+        question = "Can I ask you a support question?"
+        supportchat.send_text(question)
+        admin = admingroup.botadmin
+        botaddr = admingroup.admbot.get_config("addr")
+        admin.add_account_plugin(SupportGroupUserPlugin(admin, botaddr))
+        while len(admin.get_chats()) < 2:
+            time.sleep(0.1)
+        # AcceptChatPlugin will send 2 messages to the support group now
+        support_group_name = supportuser.get_config("addr")
+        supportgroup = next(filter(lambda chat: chat.get_name() == support_group_name,
+                                   admin.get_chats()))
+        while len(supportchat.get_messages()) < 2:
+            time.sleep(0.1)
+        botreply = supportchat.get_messages()[1]
+        assert botreply.text == "Yes of course you can ask us :)"
+        supportchat.send_text("Okay, I will think of something :)")
+        while len(supportgroup.get_messages()) < 4:
+            time.sleep(0.1)
+        assert "I hope the user can't read this" not in \
+               [msg.text for msg in supportchat.get_messages()]
+
     @pytest.mark.timeout(TIMEOUT)
     def test_invite_bot_to_group(self, admingroup, supportuser):
         botcontact = supportuser.create_contact(admingroup.admbot.get_config("addr"))
